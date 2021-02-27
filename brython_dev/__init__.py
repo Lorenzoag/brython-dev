dac37d997b9e2cc68155c76cb447855807e58b77
ECONOMIA <>
2021-02-27 05:29:09 +0000

    changes(perf): Agregando la configuracion y las primeras rutas
    FossilOrigin-Name: 43d57482f34632d815aebd97c909dd7a2b3afb9b68036f8779ec12c9b3f95dfb

--- brython_dev/__init__.py
+++ brython_dev/__init__.py
@@ -1,42 +1,66 @@
-import pkg_resources, sysconfig
-from flask import Flask, render_template, send_file, send_from_directory
-import yaml
+import sysconfig
 from pathlib import Path
 
+import pkg_resources
+import yaml
+from flask import Flask, render_template, send_file, send_from_directory, escape
+
 try:
-    __version__ = pkg_resources.get_distribution('brython-dev').version
+    __version__ = pkg_resources.get_distribution("brython-dev").version
 except pkg_resources.DistributionNotFound:
-    __version__ = 'unknown'
-    
+    __version__ = "unknown"
+
 
 def create_app(config: dict = {}) -> Flask:
     app = Flask(__name__)
-    
+
     config_file = Path("brython.yml").resolve()
     if config_file.exists():
         with open(config_file, "r") as fh:
-            app.config.from_mapping(yaml.safe_load(fh.read()))
-    
+            parse = yaml.safe_load(fh.read())
+            app.config["NAME"] = parse.get("name") or "Unnamed"
+            app.config["STYLESHEETS"] = parse.get("stylesheets") or []
+            app.config["EXTENSIONS"] = parse.get("extensions") or {}
+            app.config["USE_BRYTHON"] = app.config["EXTENSIONS"].get("brython") or True
+            app.config["USE_BRYTHON_STDLIB"] = (
+                app.config["EXTENSIONS"].get("brython_stdlib") or False
+            )
+            app.config["SCRIPTS"] = parse.get("scripts") or {}
+            brython_options = parse.get("brython_options") or {"debug": 1}
+            app.config["BRYTHON_OPTIONS"] = (
+                escape("{" + ", ".join(f"{k}: {v}" for k, v in brython_options.items()) + "}")
+            )
+            app.config["APP"] = parse.get("app") or 'app.py'
+            template = parse.get("template") or 'app.html'
+            if Path(template).exists():
+                with open(template, "r") as ft:
+                    template = ft.read()
+            else:
+                template = ""
+            app.config["TEMPLATE"] = template
+
     app.config.from_mapping(config)
-    
-    @app.route('/')
+
+    @app.route("/")
     def index():
-        return render_template('index.html')
-                                # stylesheets=app.config.get('stylesheets', []))
-                                # scripts=app.config.get('scripts', []),
-                                # extensions=app.config.get('extensions', {}),
-                                # options=app.config.get('options', {}),
-        # )
-    
+        return render_template(
+            "index.html",
+            name=app.config["NAME"],
+            stylesheets=app.config["STYLESHEETS"],
+            use_brython=app.config["USE_BRYTHON"],
+            use_brython_stdlib=app.config["USE_BRYTHON_STDLIB"],
+            scripts=app.config["SCRIPTS"],
+            brython_options=app.config["BRYTHON_OPTIONS"],
+            app=app.config["APP"],
+            template=app.config["TEMPLATE"],
+        )
+
     # @app.route("/<path:path>")
     # def catch_all(path: str):
-        # send_file(path)
-    
-    
+    # send_file(path)
 
     # @app.route('/lib/site-packages/<path:path>')
     # def site_packages(path: str):
-        # send_from_directory(sysconfig.get_path("purelib"), path)
-    
-    
+    # send_from_directory(sysconfig.get_path("purelib"), path)
+
     return app
