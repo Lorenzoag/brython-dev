8f5e32af1c99097ebeb03a7d07ac369f939a8a8d
ECONOMIA <>
2021-02-27 21:51:19 +0000

    changes(perf): Adicionando __main__.py y arreglando valores en pyproject.toml
    FossilOrigin-Name: 7c1786c4ab17e86e0fc6dbe5720e6fb8caa5046c725d75403d66aedc112c2ffe

---
+++ brython_dev/__main__.py
@@ -0,0 +1,14 @@
+import click
+from flask import Flask
+from flask.cli import FlaskGroup
+
+from brython_dev import __version__, create_app
+
+
+@click.group(cls=FlaskGroup, create_app=create_app, add_version_option=False)
+@click.version_option(version=__version__)
+def cli():
+    """Management script for brython developers."""
+
+
+cli(prog_name="Bython-dev")
