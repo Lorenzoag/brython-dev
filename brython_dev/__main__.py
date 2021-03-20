735ae61e0eb1f5af07cb8d940be5a7bb5dab6490
ECONOMIA <>
2021-03-20 03:34:53 +0000

    changes(perf): Arreglando el codigo y mejorando las pruebas
    FossilOrigin-Name: f896f70c6a61d9b39a88c49905d82de044624e04a068bc6eaaa78e7f03608e12

--- brython_dev/__main__.py
+++ brython_dev/__main__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 from flask import Flask
 from flask.cli import FlaskGroup
@@ -10,9 +12,15 @@ from brython_dev import __version__, create_app
 def cli():
     """Management script for brython developers."""
 
-@cli.command
-def init():
-    """Init"""
-    pass
 
-cli(prog_name="bython-dev")
+@cli.command()
+@click.option("--name", prompt=True, help="Proyect name")
+def init(name):  # , app, template):
+    """Creates a basic brython.yml file in the current directory."""
+
+    with Path("brython.yml") as file:
+        file.write_text(f"name: {name}")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli(prog_name="bython-dev")
--- tests/test_brython_dev.py
+++ tests/test_brython_dev.py
@@ -8,17 +8,25 @@ def test_version():
     assert __version__ == pkg_resources.get_distribution("brython-dev").version
 
 
-def test_routes(client):
-    assert client.get("/brython.js").status_code == 200
-    assert client.get("/brython_stdlib.js").status_code == 200
+@pytest.mark.parametrize(
+    ("path", "status"),
+    (
+        ("/brython.js", 200),
+        ("/brython_stdlib.js", 200),
+        ("/Lib/site-packages/easy_install.py", 200),
+        ("/Lib/site-packages/brython.py", 404),
+        ("/Lib/site-packages/brython/__init__.py", 200),
+    ),
+)
+def test_routes(client, path, status):
+    assert client.get(path).status_code == status
+    # assert client.get("/brython_stdlib.js").status_code == 200
 
-    assert client.get("/Lib/site-packages/easy_install.py").status_code == 200
-    assert client.get("/Lib/site-packages/brython.py").status_code == 404
-    assert client.get("/Lib/site-packages/brython/__init__.py").status_code == 200
+    # assert client.get("/Lib/site-packages/easy_install.py").status_code == 200
+    # assert client.get("/Lib/site-packages/brython.py").status_code == 404
+    # assert client.get("/Lib/site-packages/brython/__init__.py").status_code == 200
 
 
-def test_raise_config():
-    with pytest.raises(FileExistsError):
-        create_app(
-            {"TESTING": True, "SECRET_KEY": "dev", "CONFIG_FILE": "incorrect.yml"}
-        )
+def test_not_config_file():
+    # with pytest.raises(FileExistsError):
+    create_app({"TESTING": True, "SECRET_KEY": "dev", "CONFIG_FILE": "incorrect.yml"})
